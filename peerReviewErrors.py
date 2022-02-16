83c1a120549eab199b1da5b5b9ae844f55e89935
Tackerman1212 <99769738+Tackerman1212@users.noreply.github.com>
2022-02-16 03:39:40 -0600

    Update peerReviewErrors.py

--- peerReviewErrors.py
+++ peerReviewErrors.py
@@ -1,8 +1,8 @@
 # This is a header for the application
 # You should read this header and insert your name and your date below as part of the peer review
 # This is a typical part of any program
-# Author: <author>
-# Creation Date: <date>
+# Author: Tanner Ackerman
+# Creation Date: 2/15/2022
 # Below is a simple program with 10 issues (some are syntax errors and some are logic errors.  You need to identify the issues and correct them.
 
 import random
@@ -16,35 +16,55 @@ def displayIntro():
 	print()
 
 def chooseCave():
-    cave = ''
+   		# cave = ''
+	cave = 0
+	# Error #1: "cave" was being established as a string using quotes, it is intended to be set as an integer so initializing it to '0' is the fix. 
+	# Additionally, the line above was indented once too many, so it was moved back one indentation.
+	
 	while cave != '1' and cave != '2':
 		print('Which cave will you go into? (1 or 2)')
-		cave = input()
+		#cave = input()
+	cave = input()
+	# Error #2: The line above was indented once too many, so it was moved back one indentation.
 
-	return caves
+	#return caves
+	return cave
+	# Error #3: 'caves' should not be plural, simple syntax error. Correct version is 'cave'.
 
-def checkCave(chosenCave):
+# def checkCave(chosenCave):
+def checkCave(chooseCave):
+# Error #4: 'chosenCave' was given the past tense language, the syntax was incorrect by not matching the previously established function name of 'chooseCave'.
 	print('You approach the cave...')
 	#sleep for 2 seconds
 	time.sleep(2)
 	print('It is dark and spooky...')
 	#sleep for 2 seconds
-	time.sleep(3)
+	#time.sleep(3)
+	time.sleep(2)
+	# Error #5: '3' was entered incorrectly and was replaced with '2' which is correct according to the stated comments in the code.
 	print('A large dragon jumps out in front of you! He opens his jaws and...')
 	print()
 	#sleep for 2 seconds
 	time.sleep(2)
 	friendlyCave = random.randint(1, 2)
 
-	if chosenCave == str(friendlyCave):
+	#if chosenCave == str(friendlyCave):
+	if chosenCave == int(friendlyCave): 
+	# Error #6: 'friendlyCave' wouldn't be a string, it is an integer or 'int'.
 		print('Gives you his treasure!')
 	else:
-		print 'Gobbles you down in one bite!'
+		#print 'Gobbles you down in one bite!'
+		print('Goobles you down in one bite!')
+		# Error #7 Parenthesis were forgotten on the print() function.
 
-playAgain = 'yes'
+#playAgain = 'yes'
+playAgain = ''
+# Error #8: 'playAgain' should be initialized as an empty string with quotes, not set to 'yes' which will pass through the while statement automatically restarting the programm for the user.
 while playAgain = 'yes' or playAgain = 'y':
 	displayIntro()
-	caveNumber = choosecave()
+	#caveNumber = choosecave()
+	caveNumber = chooseCave()
+	# Error #9: syntax error by not paying being case sensitive matching the previously defined function of 'chooseCave'.
 	checkCave(caveNumber)
     
 	print('Do you want to play again? (yes or no)')
